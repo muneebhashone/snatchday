21fb65bf8b33a47c14ead51c869c3cb50f227702
HamzaHashone <hamzadev.hashone@gmail.com>
2025-04-08 14:09:46 +0500

    pull from addTocart

--- src/components/admin/CustomersList.tsx
+++ src/components/admin/CustomersList.tsx
@@ -27,257 +27,195 @@ import { Calendar } from "../ui/calendar";
 import { CustomeListTable } from "../CustomerListTable";
 
 const formSchema = z.object({
-  customerName: z.string().optional(),
+  Search: z.string().optional(),
   customerGroup: z.string().optional(),
   approved: z.string().optional(),
-  created: z.coerce.date().optional(),
-  email: z.string().optional(),
-  status: z.string().optional(),
-  IP: z.string().optional(),
+  created: z.string().optional(),
+  // created: z.coerce.date().optional(),
 });
 
 export default function CustomersList() {
   const [search, setSearch] = useState("");
   const [group, setGroup] = useState("");
   const [date, setDate] = useState("");
-  const [active, setActive] = useState("");
+  const [isApprove, setApprove] = useState("");
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      customerName: "",
+      Search: "",
       customerGroup: "",
       approved: undefined,
-      created: null,
-      email: "",
-      status: undefined,
-      IP: "",
+      created: "",
     },
   });
 
   const onSubmit = (values: any) => {
-    console.log("Form Submitted:", values);
-    setSearch(values.customerName);
+    console.log(values);
+    setSearch(values.Search.trim());
     setGroup(values.customerGroup);
     setDate(values.created);
-    setActive(values.status);
+    setApprove(values.approved);
   };
 
   const ClearFields = () => {
-    form.reset({ customerGroup: "", status: "", approved: "" });
-   
+    form.reset();
+    setSearch("");
+    setGroup("");
+    setDate("");
+    setApprove("");
   };
 
   return (
-    // <div className="max-w-full mx-auto p-6 rounded-lg bg-gray-100 pl-24">
-    <div className="max-w-full mx-auto rounded-sm border">
-      <div className="w-full pl-4 flex items-center gap-1">
-        {/* <OverviewIcon />  */}
-        <List size={18} />
-        Overview
-      </div>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-1 grid grid-cols-4 items-center justify-center gap-2 bg-gray-100 pl-24 pt-4 pb-8"
-        >
-          {/* customer name */}
-
-          <FormField
-            control={form.control}
-            name="customerName"
-            render={({ field }) => (
-              <FormItem className="">
-                <FormLabel>Search</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="Search" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          {/* customer group */}
-          <FormField
-            control={form.control}
-            name="customerGroup"
-            render={({ field }) => (
-              <FormItem className="">
-                <FormLabel>Customer Group</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  value={field.value}
-                  defaultValue=""
-                >
+    <div>
+      <h1>Customers</h1>
+      <div className="max-w-full mx-auto rounded-sm border">
+        <div className="w-full pl-4 flex items-center gap-1">
+          {/* <OverviewIcon />  */}
+          <List size={18} />
+          Overview
+        </div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-1 grid grid-cols-4 items-center justify-center gap-2 bg-gray-100 pl-24 pt-4 pb-8"
+          >
+            {/* customer name */}
+            <FormField
+              control={form.control}
+              name="Search"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel>Search</FormLabel>
                   <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select Title" />
-                    </SelectTrigger>
+                    <Input {...field} placeholder="Search customer here" />
                   </FormControl>
-                  <SelectContent>
-                    {/* <SelectItem value={null}>Select</SelectItem> */}
-                    <SelectItem value="BASIC">Basic</SelectItem>
-                    <SelectItem value="VIP">VIP</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          {/* approved */}
-          <FormField
-            control={form.control}
-            name="approved"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Approved</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  value={field.value}
-                  defaultValue=""
-                >
+            {/* customer group */}
+            <FormField
+              control={form.control}
+              name="customerGroup"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel>Customer Group</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    defaultValue=""
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select Title" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value={undefined}>Select</SelectItem>
+                      <SelectItem value="BASIC">Basic</SelectItem>
+                      <SelectItem value="VIP">VIP</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* approved */}
+            <FormField
+              control={form.control}
+              name="approved"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Approve</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    defaultValue=""
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select Status" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="true">Approved</SelectItem>
+                      <SelectItem value="false">Not Approve</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* Date */}
+            <FormField
+              control={form.control}
+              name="created"
+              render={({ field }) => (
+                <FormItem className="flex flex-col pt-3">
+                  <FormLabel>Created</FormLabel>
                   <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Approved or not" />
-                    </SelectTrigger>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-[240px] justify-start text-left font-normal",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          <CalendarIcon />
+                          {field.value
+                            ? String(field.value.split("T")[0])
+                            : "Pick a date"}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={
+                            field.value ? new Date(field.value) : undefined
+                          }
+                          onSelect={(date) => {
+                            field.onChange(
+                              date ? new Date(date).toISOString() : undefined
+                            );
+                          }}
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
                   </FormControl>
-                  <SelectContent>
-                    <SelectItem value="yes">Yes</SelectItem>
-                    <SelectItem value="no">No</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* Buttons */}
+            <div className="flex justify-start items-center pt-7 gap-4">
+              <Button
+                onClick={() => ClearFields()}
+                type="button"
+                className="ml-4 bg-[#007bff] hover:bg-[007bff]"
+              >
+                <Rotate3d />
+                Clear
+              </Button>
+              <Button type="submit" className="bg-primary hover:bg-primary">
+                <Filter />
+                Filter
+              </Button>
+            </div>
+          </form>
+        </Form>
+        <div className="px-3 my-5">
+          <CustomeListTable
+            search={search}
+            group={group}
+            date={date}
+            isActive={isApprove}
           />
-
-          <FormField
-            control={form.control}
-            name="created"
-            render={({ field }) => (
-              <FormItem className="flex flex-col pt-3">
-                <FormLabel>Created</FormLabel>
-                <FormControl>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-[240px] justify-start text-left font-normal",
-                          !field.value && "text-muted-foreground"
-                        )}
-                      >
-                        <CalendarIcon />
-                        {field.value ? String(field.value) : "Pick a date"}
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar
-                        mode="single"
-                        selected={
-                          field.value ? new Date(field.value) : undefined
-                        }
-                        onSelect={(date) => {
-                          field.onChange(
-                            date
-                              ? new Date(date).toISOString().split("T")[0]
-                              : ""
-                          );
-                        }}
-                        initialFocus
-                      />
-                    </PopoverContent>
-                  </Popover>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          {/* Email */}
-          {/* <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input type="email" {...field} placeholder="Enter email" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          /> */}
-
-          {/* status */}
-          {/* <FormField
-            control={form.control}
-            name="status"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Status</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  value={field.value}
-                  defaultValue=""
-                >
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select Status" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem disabled value={null}>
-                      Select Status
-                    </SelectItem>
-                    <SelectItem value="active">Active</SelectItem>
-                    <SelectItem value="inactive">Inactive</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          /> */}
-
-          {/* IP */}
-          {/* <FormField
-            control={form.control}
-            name="IP"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>IP</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="IP" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          /> */}
-
-          {/* Buttons */}
-          <div className="flex justify-start items-center pt-7 gap-4">
-            <Button
-              onClick={() => ClearFields()}
-              type="button"
-              className="ml-4 bg-[#007bff] hover:bg-[007bff]"
-            >
-              <Rotate3d />
-              Clear
-            </Button>
-            <Button type="submit" className="bg-primary hover:bg-primary">
-              <Filter />
-              Filter
-            </Button>
-          </div>
-        </form>
-      </Form>
-      <div className="px-3 my-5">
-        <CustomeListTable
-          search={search}
-          group={group}
-          date={date}
-          isActive={active}
-        />
+        </div>
       </div>
     </div>
   );
